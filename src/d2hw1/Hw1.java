cb26f812a6c90f365ef9bccebe7e60e5b98f8301
luisf <luisf@DESKTOP-P0D3UQL>
2023-04-07 21:19:22 -0700

    homework1

---
+++ src/d2hw1/Hw1.java
@@ -0,0 +1,13 @@
+package d2hw1;
+
+public class Hw1 {
+	public static void main(String[] args) {
+		
+}
+	public void practice1() {
+		System.out.println("Homework practice 5");
+	}
+	public void stash() {
+		System.out.println("stashing practice 5");
+	}
+}
